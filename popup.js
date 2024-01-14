b6d1edce4dbc952a1da85dc69bfa2d17d3439099
nenyatora <146788661+nenyatora@users.noreply.github.com>
2024-01-15 07:16:55 +0800

    popup.js

--- popup.js
+++ popup.js
@@ -5,29 +5,6 @@
 $("#lean_overlay").css({"display":"block",opacity:0});$("#lean_overlay").fadeTo(200,o.overlay);$(modal_id).css({"display":"block","position":"fixed","opacity":0,"z-index":11000,"left":50+"%","margin-left":-(modal_width/2)+"px","top":o.top+"px"});$(modal_id).fadeTo(200,1);e.preventDefault()})});function close_modal(modal_id){$("#lean_overlay").fadeOut(200);$(modal_id).css({"display":"none"})}}})})(jQuery);
 
 
-/*function togglepp(){
-        if (document.getElementById('dcu') && document.getElementById('uid') && document.getElementById('server') && document.getElementById('code')) {
-                document.getElementById('pp1').classList.toggle('openpp');
-        }
-        else 
-                null;
-}
-
-let isQuestionnaire1Filled = document.getElementById('dcu').value.trim() !== '';
-let isQuestionnaire2Filled = document.getElementById('uid').value.trim() !== '';
-let isQuestionnaire3Filled = document.getElementById('server').value.trim() !== '';
-let isQuestionnaire4Filled = document.getElementById('code').value.trim() !== '';
-
-
-if (isQuestionnaire1Filled && isQuestionnaire2Filled && isQuestionnaire3Filled && isQuestionnaire4Filled) {
-
-    document.getElementById('pp1').classList.toggle('openpp');
-} else {
-    // Code to handle the case when not all questionnaires are filled
-    null;
-}*/
-
-
 document.getElementById('form-submit-1').addEventListener('click', function(event) {
     event.preventDefault();
     togglepp();
